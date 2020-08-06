7aba503bc377e0285cade1d03749f425f435bae3
Ray Phillips <raydphillips3@gmail.com>
2020-08-06 08:16:01 -0500

    Updates

---
+++ precache-manifest.e37201b1ef0c66846506af58f630b66f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b745f7068277f32da80516b35d92ebd",
+    "url": "/Portfolio_React/index.html"
+  },
+  {
+    "revision": "53c3f197cf16f19f5b67",
+    "url": "/Portfolio_React/static/css/main.129b95ee.chunk.css"
+  },
+  {
+    "revision": "f156be7cb7b733f91a4d",
+    "url": "/Portfolio_React/static/js/2.c2c80342.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio_React/static/js/2.c2c80342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c3f197cf16f19f5b67",
+    "url": "/Portfolio_React/static/js/main.ee2064aa.chunk.js"
+  },
+  {
+    "revision": "3b86a768e662e9a869ff",
+    "url": "/Portfolio_React/static/js/runtime-main.feb04364.js"
+  },
+  {
+    "revision": "f2b95a6669c18e5892a5e53b0ad862cc",
+    "url": "/Portfolio_React/static/media/resumePage1.f2b95a66.jpg"
+  },
+  {
+    "revision": "7d1069123ae52e93f92d9139bd3be45d",
+    "url": "/Portfolio_React/static/media/resumePage2.7d106912.jpg"
+  }
+]);
